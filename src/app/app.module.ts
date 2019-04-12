405a8ba1a77f8149988a2128d6c2f335a299a27d
shantanu301 <dshantanu1989@gmail.com>
2019-04-12 23:19:16 +0530

    adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { FreeComponent } from './pricing/free.component';
+import { ProComponent } from './pricing/pro.component';
+import { EnterpriseComponent } from './pricing/enterprise.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    FooterComponent,
+    PricingComponent,
+    FreeComponent,
+    ProComponent,
+    EnterpriseComponent,
+    
   ],
   imports: [
     BrowserModule
